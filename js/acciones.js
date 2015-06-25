3af556a353c1947c959fd352d277c5b26f58f539
AL01F13 <fernanda.aguilarj@gmail.com>
2015-06-25 11:09:18 -0500

    uno
    asdfghjk

---
+++ js/acciones.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnmenu').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btnmiembros').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btnhistoria').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btngenero').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btnpremios').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btnseparacion').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btndespues').on ('tap',function(){
+		navigator.vibrate(4000);
+	$('#btnprincipal').on ('tap',function(){
+		navigator.vibrate(4000);
+});
+});
+});
+});
+});
+});
+});
+});
+	$('#btnmenu').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btnmiembros').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btnhistoria').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btngenero').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btnpremios').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btnseparacion').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btndespues').on('tap',function(){
+		navigator.notification.beep(1);
+	$('#btnprincipal').on('tap',function(){
+		navigator.notification.beep(1);
+		
+});
+});
+});
+});
+});
+});
+});
+});	
+	
+}); 
+});
+
